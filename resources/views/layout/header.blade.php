daaba19c77116a29c977af7452d77e8c23c583d9
perry623 <kadekperry86@gmail.com>
2023-05-24 09:17:14 +0700

    Vite and view Setup

---
+++ resources/views/layout/header.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+@vite(['resources/js/app.js'])
+<body>
+    <div class="container">
+        <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
+          <div>Giant Book Supplier</div>
+        </header>
+      </div>
+</body>
+</html>
